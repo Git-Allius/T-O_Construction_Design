47b87e047b08c2c7707722370cdd755a68ebc8e3
AlexCuadros <cuadrosal88@gmail.com>
2026-01-15 18:40:51 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var MenuItems = document.getElementById("MenuItems");
+
+    if (MenuItems.style.display === "block") {
+        MenuItems.style.display = "none";
+    } else {
+        MenuItems.style.display = "block";
+    }
+}
